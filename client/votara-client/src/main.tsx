26afdcbd2e168e9dc145486bcf1b01dde494f0c4
adityagupta0251 <itsokkadi0251@gmail.com>
2026-05-11 19:40:38 +0000

    finalized things

--- client/votara-client/src/main.tsx
+++ client/votara-client/src/main.tsx
@@ -1,6 +1,7 @@
 import { Buffer } from "buffer";
 import process from "process";
 import "./polyfills";
+
 globalThis.Buffer = Buffer;
 globalThis.process = process;
 
@@ -13,18 +14,24 @@ import { HelmetProvider } from "react-helmet-async";
 
 import { WalletContext } from "./wallet";
 import { ProgramProvider } from "./program";
+import { ConnectionProvider } from "../src/connection"; // ✅ ADD THIS
+
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <HelmetProvider>
-            <WalletContext>
-                <ProgramProvider>
-                    <BrowserRouter>
-                        <App />
-                    </BrowserRouter>
-                </ProgramProvider>
-            </WalletContext>
+            <ConnectionProvider>
+                {" "}
+                {/* ✅ ADD THIS WRAPPER */}
+                <WalletContext>
+                    <ProgramProvider>
+                        <BrowserRouter>
+                            <App />
+                        </BrowserRouter>
+                    </ProgramProvider>
+                </WalletContext>
+            </ConnectionProvider>
         </HelmetProvider>
     </StrictMode>,
 );
